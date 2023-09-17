4c951e2582cb967f90831649431bd744b1af6c71
Marcus Roza <marcusroza@pop-os.localdomain>
2023-09-17 01:40:44 -0300

    up rota /

--- src/app.controler.ts
+++ src/app.controler.ts
@@ -5,7 +5,7 @@ export class MessageController {
   @Get()
   message() {
     const swaggerLink =
-      'https://app.swaggerhub.com/apis-docs/MAVIROLERO/back-to_me_api/1.0.0';
-    return `Bem-vindo à API BackToMe. , acesse  nossa documentação em: <a href="${swaggerLink}">BackToMe.</a>`;
+      'https://app.swaggerhub.com/apis-docs/MAVIROLERO/relicario/1.0.0';
+    return `Bem-vindo à API RelicarioApp , acesse  nossa documentação em: <a href="${swaggerLink}">Relicario.</a>`;
   }
 }
